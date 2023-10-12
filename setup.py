8ba4ddaa1f49695e08bc88e525dc3c68da460ff1
Phil Elson <philip.elson@cern.ch>
2023-10-12 10:26:06 +0200

    Repository copy at 69f14f2fce0e0022ce4539b53c710aea21285608

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+# Copyright (C) 2023, CERN
+# This software is distributed under the terms of the MIT
+# licence, copied verbatim in the file "LICENSE".
+# In applying this license, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as Intergovernmental Organization
+# or submit itself to any jurisdiction.
+
 from pathlib import Path
 
 from setuptools import find_packages, setup
@@ -14,7 +21,6 @@ REQUIREMENTS: dict[str, list[str]] = {
         "gunicorn",
         "packaging",
         "uvicorn[standard]",
-        "cmw-tracing",
         "pydantic-settings",
         "simple-repository",
     ],
